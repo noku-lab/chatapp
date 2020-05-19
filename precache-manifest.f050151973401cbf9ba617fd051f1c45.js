4d082be49aca5b895bcbed86d30c1e4d96bf3256
noku-lab <61013631+noku-lab@users.noreply.github.com>
2020-05-19 22:54:16 +0200

    Updates

---
+++ precache-manifest.f050151973401cbf9ba617fd051f1c45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2049230e4f91529cb7824dd74dd66d77",
+    "url": "/chatapp/index.html"
+  },
+  {
+    "revision": "a3e843cd398bb98dd8da",
+    "url": "/chatapp/static/css/main.a7ac3b8d.chunk.css"
+  },
+  {
+    "revision": "7724d6099d04e4003c70",
+    "url": "/chatapp/static/js/2.37ec7676.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chatapp/static/js/2.37ec7676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e843cd398bb98dd8da",
+    "url": "/chatapp/static/js/main.62b7c0cf.chunk.js"
+  },
+  {
+    "revision": "84f5fcca3f40b227a3fd",
+    "url": "/chatapp/static/js/runtime-main.a7610a6a.js"
+  }
+]);
