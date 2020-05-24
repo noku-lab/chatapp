840878255aa3c6395b76b8cb739812a341dc02c7
noku-lab <61013631+noku-lab@users.noreply.github.com>
2020-05-24 23:50:53 +0200

    colors

--- src/components/TextContainer/TextContainer.js
+++ src/components/TextContainer/TextContainer.js
@@ -8,14 +8,14 @@ const TextContainer = ({ users }) => (
   <div className="textContainer">
     <div>
       <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
-      <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
-      <h2>Try it out right now! <span role="img" aria-label="emoji">⬅️</span></h2>
+      <h2>Created with React, Express, Node and Socket.io <span role="img" aria-label="emoji">❤️</span></h2>
+      <h2>Check it out <span role="img" aria-label="emoji">⬅️</span></h2>
     </div>
     {
       users
         ? (
           <div>
-            <h1>People currently chatting:</h1>
+            <h1>People currently online:</h1>
             <div className="activeContainer">
               <h2>
                 {users.map(({name}) => (
